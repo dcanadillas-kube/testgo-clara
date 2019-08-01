4dad2c5c05a8567025aac3a121944ea3e46cee50
dcanadillas <dcanadillas@cloudbees.com>
2019-08-01 18:59:07 +0200

    New CJXD message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "CloudBees Jenkins X Distribution"
 
 	from := ""
 	if r.URL != nil {
